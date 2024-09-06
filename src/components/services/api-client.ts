9205cd648f35927a1aa2e63e956fd222d05006a2
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-09-06 09:58:50 +0600

    GH: create custom hook to fetch the games

--- src/components/services/api-client.ts
+++ src/components/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-axios.create({
+export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: '7e2bbf72caf4423dac9046ae149979a7'
