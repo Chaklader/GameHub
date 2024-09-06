a4d01cc6cd9a3a3ff6f55dd0222fe02936ee6409
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-09-06 11:00:12 +0600

    GH: building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,9 +9,9 @@ const GameGrid = () => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={3} spacing={10}>
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
                 {games.map(game => (
-                    <GameCard game={game}/>
+                    <GameCard key={game.id} game={game}/>
                 ))}
             </SimpleGrid>
         </>
