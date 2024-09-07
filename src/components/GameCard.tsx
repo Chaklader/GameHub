e51eb5ec6cc377a1c54bec596711885714b2a2f7
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-09-07 17:37:13 +0600

    GH: added Game card skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,15 +10,15 @@ interface Props {
 
 const GameCard = ({game}: Props) => {
     return (
-        <Card borderRadius='10px' overflow='hidden'>
+        <Card width='300px' borderRadius='10px' overflow='hidden'>
             <Image src={getCroppedImageURL(game.background_image)}/>
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <HStack justifyContent={'space-between'}>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+                    <CriticScore score={game.metacritic}/>
+                </HStack>
             </CardBody>
-            <HStack justifyContent={'space-between'}>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
-                <CriticScore score={game.metacritic}/>
-            </HStack>
         </Card>
     );
 };
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,23 @@
 import {SimpleGrid, Text} from "@chakra-ui/react";
-import useGames from "src/hooks/useGames.ts";
 import GameCard from "src/components/GameCard.tsx";
+import GameCardSkeleton from "src/components/GameCardSkeleton.tsx";
+import useGames from "src/hooks/useGames.ts";
 
 
 const GameGrid = () => {
+    const {games, error, isLoading} = useGames();
+    const skeletons = [1, 2, 3, 4, 5, 6];
 
-    const {games, error} = useGames();
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding='10px' spacing={10}>
-                {games.map(game => (
+            <SimpleGrid
+                columns={{sm: 1, md: 2, lg: 3, xl: 5}}
+                padding="10px"
+                spacing={10}
+            >
+                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+                {games.map((game) => (
                     <GameCard key={game.id} game={game}/>
                 ))}
             </SimpleGrid>
