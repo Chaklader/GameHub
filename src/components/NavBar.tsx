8104d812addab0895d0abfe0bfeb94a1b2d05842
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-09-08 13:49:34 +0600

    GH: search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import {HStack, Image} from "@chakra-ui/react";
 // @ts-ignore
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from "src/components/ColorModeSwitch.tsx";
+import SearchInput from "src/components/SearchInput.tsx";
 
 const NavBar = () => {
     return (
-        <HStack justifyContent='space-between' padding='10px'>
+        <HStack padding='10px'>
             <Image src={logo} boxSize='60px'/>
+            <SearchInput/>
             <ColorModeSwitch/>
         </HStack>
     );
