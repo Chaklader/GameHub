1ec78e0773530b6403d0b865d0ba8c9c119a2f2d
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2024-09-10 12:46:00 +0600

    init game details page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,15 @@
 import {createBrowserRouter} from "react-router-dom";
 import Layout from "src/pages/Layout.tsx";
 import HomePage from "src/pages/HomePage.tsx";
+import GameDetailPage from "src/pages/GameDetailPage.tsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>,
         children: [
-            {index: true, element: <HomePage/>}
+            {index: true, element: <HomePage/>},
+            {path: 'games/:id', element: <GameDetailPage/>}
         ]
     }
 ]);
